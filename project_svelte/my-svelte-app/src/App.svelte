9b7763fcc50d71e082ad185228052268cfd7e668
Filippenko Dmitry <dmitfilippenko.work@gmail.com>
2024-10-25 14:48:00 +0400

    переписал css app, структурировал стили

--- project_svelte/my-svelte-app/src/App.svelte
+++ project_svelte/my-svelte-app/src/App.svelte
@@ -45,283 +45,206 @@
   </script>
 
   <style>
-    body {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-      color: #333;
-      transition: background-color 0.8s, color 0.8s;
-    }
-
-    body[data-theme='light'] {
-      background-color: #f0f0f0;
-      color: #333333;
-    }
-
-    body[data-theme='dark'] {
-      background-color: #2b2b2b;
-      color: #f0f0f0;
-    }
-
-    .app-container {
-      display: flex;
-      min-height: 100vh;
-    }
-
-    .container_text {
-    background: linear-gradient(135deg, #e0f7fa, #00796b); /* Градиентный фон */
-    padding: 20px;
-    border-radius: 15px; /* Закругленные углы */
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15); /* Тень для объема */
-    margin-top: 20px;
-    animation: fadeIn 0.8s ease-out; /* Плавное появление */
-    color: white; /* Белый цвет текста для контраста */
-    font-size: 1.1em;
-    line-height: 1.5; /* Улучшает читаемость текста */
-    max-width: 800px;
-    margin: 20px auto; /* Центрирование блока */
+      body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    color: #333;
+    transition: background-color 0.8s, color 0.8s;
   }
 
-    /* Текстовые элементы внутри контейнера */
+  body[data-theme='light'] {
+    background-color: #f0f0f0;
+    color: #333333;
+  }
+
+  body[data-theme='dark'] {
+    background-color: #2b2b2b;
+    color: #f0f0f0;
+  }
+
+  .container_text {
+    background: linear-gradient(135deg, #e0f7fa, #00796b);
+    padding: 20px;
+    border-radius: 15px;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+    animation: fadeIn 0.8s ease-out;
+    color: white;
+    font-size: 1.1em;
+    line-height: 1.5;
+    max-width: 800px;
+    margin: 20px auto;
+  }
+
   .container_text h1 {
     font-size: 2em;
     margin-bottom: 15px;
-    color: #ffffff; /* Белый цвет заголовка */
-    text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.4); /* Тень для заголовка */
-  }
-
-  .container_text p {
-    margin-bottom: 10px;
-    color: #e0f7fa; /* Светлый оттенок для текста */
+    color: #ffffff;
+    text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.4);
   }
 
   .container_text a {
-    color: #ffeb3b; /* Жёлтые ссылки */
+    color: #ffeb3b;
     text-decoration: underline;
     transition: color 0.3s ease;
   }
 
   .container_text a:hover {
-    color: #ffca28; /* Изменение цвета при наведении */
+    color: #ffca28;
   }
 
   .menu {
-    position: fixed; /* Закрепляем меню относительно окна браузера */
+    position: fixed;
     top: 90px;
-    left: -370px; /* Скрываем меню за пределами экрана */
-    width: 200px; /* Фиксированная ширина меню */
-    height: 100%; /* Меню занимает всю высоту экрана */
+    left: -370px;
+    width: 200px;
+    height: 100%;
     overflow: hidden;
     background-color: #004d40;
     color: white;
-    transition: left 0.3s ease; /* Анимация появления меню */
-    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.5); /* Добавляем тень */
-    z-index: 9; /* Меню находится поверх контента */
+    transition: left 0.3s ease;
+    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.5);
+    z-index: 9;
   }
 
   .menu.open {
-    left: 0; /* При открытии меню появляется на экране */
+    left: 0;
   }
 
-    /* .menu.open {
-      width: 740px;
-    } */
+  .menu ul {
+    list-style: none;
+    padding: 20px;
+    margin: 0;
+  }
 
-    .menu ul {
-      list-style: none;
-      padding: 20px;
-      margin: 0;
-    }
+  .menu ul li a {
+    color: #e0f7fa;
+    font-size: 18px;
+    text-decoration: none;
+    display: block;
+    padding: 10px;
+    transition: background-color 0.3s;
+    border-radius: 4px;
+  }
 
-    .menu ul li {
-      margin: 20px 0;
-    }
+  .menu ul li a:hover {
+    background-color: #00796b;
+  }
 
-    .menu ul li a {
-      color: #e0f7fa;
-      font-size: 18px;
-      text-decoration: none;
-      display: block;
-      padding: 10px;
-      transition: background-color 0.3s;
-      border-radius: 4px;
-    }
+  .header {
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
 
-    .menu ul li a:hover {
-      background-color: #00796b;
-    }
+  .logo {
+    width: 50px;
+    height: auto;
+    margin-right: 15px;
+  }
 
-    .content {
-      flex-grow: 1;
-      padding: 20px;
-      transition: margin-left 0.3s ease;
-    }
+  .title {
+    font-size: 24px;
+    color: #333;
+  }
 
+  button {
+    transition: background-color 0.3s;
+    background-color: #f4f4f4;
+    border: 1px solid #ccc;
+    padding: 10px 15px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 
-    /* при открытии меню сдвигать content вправо */
-
-    /*
-    .content.menu-open {
-      margin-left: 250px;
-    }
-    */ 
-
-
-    .header {
-      /* background-color: #e0f7fa; */
-      padding: 10px 20px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: relative;
-      /* border-bottom: 2px solid #00796b; */
-    }
-
-    .logo {
-      width: 50px;
-      height: auto;
-      margin-right: 15px;
-    }
-
-    .title {
-      font-size: 24px;
-      color: #333;
-    }
-
-    /* .theme-toggle {
+  .menu-button {
+    position: absolute;
+    top: 30px;
+    left: 20px;
+    padding: 10px 20px;
     background-color: #00796b;
     color: white;
+    font-size: 16px;
     border: none;
-    padding: 10px 15px;
     cursor: pointer;
     border-radius: 4px;
     transition: background-color 0.3s;
-  } */
+    z-index: 10;
+  }
 
-  .theme-toggle:hover {
+  .menu-button:hover {
     background-color: #004d40;
   }
 
-    button {
-      transition: background-color 0.3s;
-      background-color: #f4f4f4;
-      border: 1px solid #ccc;
-      padding: 10px 15px;
-      border-radius: 4px;
-      cursor: pointer;
-    }
+  .content {
+    padding-top: 150px;
+    flex-grow: 1;
+    padding: 20px;
+  }
 
-    button:hover {
-      background-color: #ddd;
-    }
+  .chat-button {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background-color: #00796b;
+    color: white;
+    border: none;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    z-index: 10;
+  }
 
-    .menu-button {
-      position: absolute;
-      top: 30px;
-      left: 20px;
-      padding: 10px 20px;
-      background-color: #00796b;
-      color: white;
-      font-size: 16px;
-      border: none;
-      cursor: pointer;
-      border-radius: 4px;
-      transition: background-color 0.3s;
-      z-index: 10;
-    }
+  .chat-button:hover {
+    background-color: #004d40;
+  }
 
-    .menu-button:hover {
-      background-color: #004d40;
-    }
+  .chat-window {
+    position: fixed;
+    bottom: 100px;
+    right: 20px;
+    width: 300px;
+    height: 400px;
+    background-color: #e0f7fa;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: none;
+    z-index: 20;
+    padding: 20px;
+    color: #333;
+  }
 
-    .container {
-      padding: 20px;
-      flex-grow: 1;
-    }
+  .chat-window.open {
+    display: block;
+  }
 
-    .chat-button {
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      background-color: #00796b;
-      color: white;
-      border: none;
-      border-radius: 50%;
-      width: 60px;
-      height: 60px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;  
-      transition: background-color 0.3s;
-      z-index: 10;
-    }
+  .chat-header {
+    font-size: 18px;
+    color: #00796b;
+    margin-bottom: 10px;
+  }
 
-    .chat-button:hover {
-      background-color: #004d40;
-    }
+  .chat-options button {
+    background-color: #00796b;
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    cursor: pointer;
+    color: white;
+    transition: background-color 0.3s;
+  }
 
-    .chat-window {
-      position: fixed;
-      bottom: 100px;
-      right: 20px;
-      width: 300px;
-      height: 400px;
-      background-color: #e0f7fa;
-      border-radius: 10px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      display: none;
-      z-index: 20;
-      padding: 20px;
-      color: #333;
-    }
-
-    .chat-window.open {
-      display: block;
-    }
-
-    .chat-header {
-      font-size: 18px;
-      color: #00796b;
-      margin-bottom: 10px;
-    }
-
-    .chat-options {
-      display: flex;
-      flex-direction: column;
-    }
-
-    .chat-options button {
-      background-color: #00796b;
-      border: none;
-      padding: 10px;
-      border-radius: 5px;
-      margin-bottom: 10px;
-      cursor: pointer;
-      color: white;
-      transition: background-color 0.3s;
-    }
-
-    .chat-options button:hover {
-      background-color: #004d40;
-    }
-
-    @keyframes fadeIn {
-      from {
-        opacity: 0;
-        transform: translateY(10px);
-      }
-      to {
-        opacity: 1;
-        transform: translateY(0);
-      }
-    }
-
-
-    /* Основные стили для фиксированного меню */
-    * {
-    box-sizing: border-box; /* Apply box-sizing to all elements */
+  .chat-options button:hover {
+    background-color: #004d40;
   }
 
   nav {
@@ -329,52 +252,35 @@
     top: 0;
     left: 0;
     width: 100%;
+    height: 60px;
     background-color: rgba(255, 255, 255, 0.9);
     backdrop-filter: blur(10px);
     z-index: 1000;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    padding: 10px 15px; /* Adjusted padding */
+    padding: 10px 15px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    overflow: hidden; /* Prevent overflow */
   }
-
 
   nav .header {
     display: flex;
     align-items: center;
   }
 
-  .header img.logo {
-    height: 50px; /* Размер логотипа */
-    margin-right: 15px;
-  }
-
-  .header h1.title {
-    font-size: 24px;
-    margin: 0;
-    color: #000;
-  }
-
-  /* Стили для меню */
   nav ul {
     list-style: none;
     display: flex;
     gap: 20px;
     margin: 0;
     padding: 0;
-    justify-content: center; /* Центрирование элементов меню */
-    flex: 1; /* Заставляет меню занять центральное положение */
-  }
-
-  nav li {
-    display: inline-block;
+    justify-content: center;
+    flex: 1;
   }
 
   nav a {
     text-decoration: none;
-    color: #000; /* Цвет текста */
+    color: #000;
     font-size: 18px;
     padding: 10px 15px;
     border-radius: 5px;
@@ -383,15 +289,8 @@
 
   nav a:hover,
   nav a.active {
-    background-color: #00bfa6; /* Цвет при наведении и активная ссылка */
+    background-color: #00bfa6;
     color: #fff;
-  }
-
-  /* Размещение кнопки переключения темы */
-  nav .theme-toggle-container {
-    display: flex;
-    align-items: center;
-    margin-left: auto; /* Automatically push the button to the right */
   }
 
   .theme-toggle {
@@ -401,16 +300,18 @@
     padding: 10px 15px;
     cursor: pointer;
     border-radius: 4px;
-    transition: background-color 0.3s;
-    white-space: nowrap; /* Prevent the button from wrapping */
   }
 
-  /* Отступ для основного контента */
-  .content {
-    padding-top: 150px; /* Отступ под фиксированное меню */
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
-
-    
 
   </style>
 
