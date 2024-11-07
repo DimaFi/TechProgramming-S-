cf848ab647ac883d2da0363aefc8e0cfc8c192b7
Vyacheslav Sinew <vyacheslav.sinew@yandex.ru>
2024-11-07 11:57:59 +0400

    2024-11-07 Добавил рабочую версию с страницей Новости

--- kind_soul/main/admin.py
+++ kind_soul/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')  # Поля для отображения в админке
+    search_fields = ('title',)  # Поле поиска
